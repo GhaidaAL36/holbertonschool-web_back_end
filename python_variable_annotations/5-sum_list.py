73e9a681a171bc47a3f33711f889d9a709937c9a
GhaidaAl36 <ghaida3638@gmail.com>
2026-02-15 15:09:52 +0300

    task 5

---
+++ python_variable_annotations/5-sum_list.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""
+type-annotated function return float sum
+of list of float
+"""
+
+
+def sum_list(input_list: list) -> float:
+    """
+    return the sum of float list
+    """
+    return sum(input_list)
