55dd7fb0a06e74094cb70387d97917ee579fd1da
GhaidaAl36 <ghaida3638@gmail.com>
2026-02-15 14:43:19 +0300

    Pycodestyle fix

--- python_variable_annotations/0-add.py
+++ python_variable_annotations/0-add.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
 """
-type-annotated function that adds and returns float 
+type-annotated function that adds and returns float
 """
+
 
 def add(a: float, b: float) -> float:
     """
