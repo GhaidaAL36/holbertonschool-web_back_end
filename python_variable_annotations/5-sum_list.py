3087e44b1f2f84ab305a7728a39942dc43876bd0
GhaidaAl36 <ghaida3638@gmail.com>
2026-02-15 15:11:00 +0300

    task 5

--- python_variable_annotations/5-sum_list.py
+++ python_variable_annotations/5-sum_list.py
@@ -3,9 +3,10 @@
 type-annotated function return float sum
 of list of float
 """
+from typing import List
 
 
-def sum_list(input_list: list) -> float:
+def sum_list(input_list: List[float]) -> float:
     """
     return the sum of float list
     """
