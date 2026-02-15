d2c422ec350a6bf17a0dca31daa26abb19a912a3
GhaidaAl36 <ghaida3638@gmail.com>
2026-02-15 14:55:26 +0300

    task 2

---
+++ python_variable_annotations/2-floor.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""
+type-annotated function takes float
+and returns the floor of float
+"""
+import math
+
+
+def floor(n: float) -> int:
+    """
+    takes float and return the floor
+    """
+    return math.floor(n)
