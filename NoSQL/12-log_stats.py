968eaa2e79a247449e17cf142bbf5ccf34f63186
GhaidaAl36 <ghaida3638@gmail.com>
2026-03-06 22:06:37 +0300

    all

--- NoSQL/12-log_stats.py
+++ NoSQL/12-log_stats.py
@@ -1,23 +1,19 @@
 #!/usr/bin/env python3
-""" Log stats script - strict output matching """
+"""12-log_stats.py"""
 from pymongo import MongoClient
-
 
 if __name__ == "__main__":
     client = MongoClient('mongodb://127.0.0.1:27017')
-    db_nginx = client.logs.nginx
+    nginx = client.logs.nginx
+
+    total_logs = nginx.count_documents({})
+    print(f"{total_logs} logs")
+
+    print("Methods:")
     methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
+    for m in methods:
+        count = nginx.count_documents({"method": m})
+        print(f"\tmethod {m}: {count}")
 
-    count_logs = db_nginx.count_documents({})
-    print(f'{count_logs} logs')
-
-    print('Methods:')
-    for method in methods:
-        count_method = db_nginx.count_documents({'method': method})
-        print(f'\tmethod {method}: {count_method}')
-
-    check = db_nginx.count_documents(
-        {"method": "GET", "path": "/status"}
-    )
-
-    print(f'{check} status check')
+    status_check = nginx.count_documents({"method": "GET", "path": "/status"})
+    print(f"{status_check} status check")
